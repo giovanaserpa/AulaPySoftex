e22853875ae84f734c96ebb3a8f6bffe977107cf
Giovana Serpa <gel_serpa@hotmail.com>
2025-08-18 18:30:39 -0300

    Delete 8.py

--- 8.py
+++
@@ -1,5 +0,0 @@
-texto = "programação"
-texto_invertido = texto[::-1]
-igual = texto == texto_invertido
-print("String invertida:", texto_invertido)
-print("A string original é igual à invertida?", igual)
